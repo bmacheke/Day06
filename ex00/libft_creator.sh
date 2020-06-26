6debe29868e7a1b9897993966c56d246245f4093
bmacheke <67003918+bmacheke@users.noreply.github.com>
2020-06-26 12:11:29 +0200

    Add files via upload

---
+++ ex00/libft_creator.sh
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void 	ft_putchar(char c);
+void	swap(int *a, int *b);
+void	ft_putstr(char *str);
+void	ft_strlen(char *str);
+void	ft_strcmp(char *s1, char *s2);
